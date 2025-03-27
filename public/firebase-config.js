8029835935ae97d32643a111d5e836c654774def
Brune <168581255+BruneCoding@users.noreply.github.com>
2025-03-27 19:30:49 +0000

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,9 +1,9 @@
-// This will be automatically populated by Firebase when deployed
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAPS6tBf7F4L5ZDh2NPQ5uv9ToZDcID61U",
+  authDomain: "brungle-proxy-testing.firebaseapp.com",
+  projectId: "brungle-proxy-testing",
+  storageBucket: "brungle-proxy-testing.firebasestorage.app",
+  messagingSenderId: "943408220731",
+  appId: "1:943408220731:web:5c6d37842f734a87d5ea1e",
+  measurementId: "G-JRV6Q9YRJL"
 };
